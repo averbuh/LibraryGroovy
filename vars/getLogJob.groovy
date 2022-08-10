d71cbb5a4bfd2e4bb38a255d360affff04e55843
Averbuh Alex <averbuh.alex.in@gmail.com>
2022-08-10 23:54:13 +0300

    ...

--- vars/getLogJob.groovy
+++ vars/getLogJob.groovy
@@ -2,5 +2,5 @@ def call(int lines, String jobName, String buildNumber) {
     def jenkins = Jenkins.getInstance()
     def job = jenkins.getItem(jobName)
     def bld = job.getBuildByNumber(buildNumber.toInteger())
-    return bld.getLog(lines)
+    return bld.getLog(lines).logFile.text
 }
